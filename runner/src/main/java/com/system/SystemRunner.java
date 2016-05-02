9b4fbda31ce3ea74499fb649cbeb4c7a55cc9220
Andrew <ampopp04@gmail.com>
2016-05-02 15:02:35 +0000

    Create Runner

--- runner/src/main/java/com/system/SystemRunner.java
+++ runner/src/main/java/com/system/SystemRunner.java
@@ -3,7 +3,7 @@ package com.system;
 import com.system.util.date.DateUtils;
 
 public class SystemRunner {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World! Today is " + DateUtils.getToday());
     }
 }
