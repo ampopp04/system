e0ccee2407f53cbd29f89790bb2eb0ddb0714879
Andrew <ampopp04@gmail.com>
2016-05-03 16:50:21 +0000

    Add CollectionUtils test

--- core-util/src/test/java/com/system/util/collection/CollectionUtilsTest.java
+++ core-util/src/test/java/com/system/util/collection/CollectionUtilsTest.java
@@ -1,12 +1,25 @@
 package com.system.util.collection;
 
-import org.junit.*;
 import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
 
 public class CollectionUtilsTest {
 
     @Test
     public void iteratorNotNullTest() {
         assertNotNull(CollectionUtils.getIterator(null));
+    }
+    
+    @Test
+    public void iteratorExistsTest(){
+      List<String> stringList = new ArrayList<>();
+      stringList.add("test1");
+      stringList.add("test2");
+      
+      assertNotNull(CollectionUtils.getIterator(stringList));
     }
 }
