d6ebd1557eda04486d7c7adab473c0596cc33a4c
Andrew <ampopp04@gmail.com>
2016-05-06 06:51:22 -0500

    Remove Maven Support

--- runner/src/main/java/com/system/SystemRunner.java
+++ runner/src/main/java/com/system/SystemRunner.java
@@ -12,9 +12,9 @@ public class SystemRunner {
     public static void main(String[] args) {
         System.out.println("Hello World! Today is " + DateUtils.getToday());
         ApplicationContext ctx = SpringApplication.run(SystemRunner.class, args);
-        
+
         System.out.println("Let's inspect the beans provided by Spring Boot:");
-        
+
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
