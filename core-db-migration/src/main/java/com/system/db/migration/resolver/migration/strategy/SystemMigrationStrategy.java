12a9dcaa8e48d8385433f13b722b01ee9f99ca85
Andrew <ampopp04@gmail.com>
2016-05-31 10:08:20 -0500

    Add Table Creation Callback

---
+++ core-db-migration/src/main/java/com/system/db/migration/resolver/migration/strategy/SystemMigrationStrategy.java
@@ -0,0 +1,26 @@
+package com.system.db.migration.resolver.migration.strategy;
+
+import com.system.db.migration.resolver.callback.ApplicationContextAwareSpringJdbcCallbackResolver;
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.ApplicationContext;
+
+/**
+ * The <class>SystemMigrationStrategy</class> defines the migration
+ * strategy to use for execution system migrations
+ *
+ * @author Andrew
+ */
+public class SystemMigrationStrategy implements FlywayMigrationStrategy {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Override
+    public void migrate(Flyway flyway) {
+        flyway.setSkipDefaultCallbacks(true);
+        flyway.setCallbacks(new ApplicationContextAwareSpringJdbcCallbackResolver(context).resolveCallbacks());
+        flyway.migrate();
+    }
+}
