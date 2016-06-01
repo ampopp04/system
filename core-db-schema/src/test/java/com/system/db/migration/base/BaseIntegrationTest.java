1835be02bc49b525b5da7e8851442275636817e8
Andrew <ampopp04@gmail.com>
2016-06-01 08:03:29 -0500

    Update Tests

--- core-db-schema/src/test/java/com/system/db/migration/base/BaseIntegrationTest.java
+++ core-db-schema/src/test/java/com/system/db/migration/base/BaseIntegrationTest.java
@@ -1,10 +1,14 @@
 package com.system.db.migration.base;
 
 import com.system.inversion.InversionContainer;
+import com.system.inversion.intercept.SimpleClasspathMetadataReaderFactory;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.core.type.classreading.SimpleMetadataReaderFactory;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static com.system.manipulator.interceptor.util.InterceptorUtils.classInterceptor;
 
 /**
  * The <class>BaseIntegrationTest</class> defines the
@@ -17,4 +21,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringApplicationConfiguration(InversionContainer.class)
 @WebIntegrationTest(randomPort = true)
 public abstract class BaseIntegrationTest {
+    static {
+        classInterceptor(SimpleMetadataReaderFactory.class, SimpleClasspathMetadataReaderFactory.class);
+    }
 }
