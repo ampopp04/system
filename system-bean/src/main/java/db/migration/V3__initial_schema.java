40076de8ea83070a1895f3c7218afe66aa4f9fd8
Andrew <ampopp04@gmail.com>
2016-06-08 19:40:35 -0500

    Add SystemBean support

--- system-bean/src/main/java/db/migration/V3__initial_schema.java
+++ system-bean/src/main/java/db/migration/V3__initial_schema.java
@@ -11,6 +11,7 @@ import com.system.bean.variable.definition.modifier.type.SystemBeanVariableDefin
 import com.system.db.entity.Entity;
 import com.system.db.migration.table.TableCreationMigration;
 import com.system.db.repository.base.entity.SystemRepository;
+import com.system.db.repository.base.named.NamedEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
@@ -34,6 +35,12 @@ public class V3__initial_schema extends TableCreationMigration {
     @Autowired
     private SystemRepository<SystemBeanDefinitionType> systemBeanDefinitionTypeRepository;
 
+    @Autowired
+    private NamedEntityRepository<SystemBeanModifierType> systemBeanModifierTypeRepository;
+
+    @Autowired
+    private NamedEntityRepository<SystemBeanDefinition> systemBeanDefinitionRepository;
+
     ///////////////////////////////////////////////////////////////////////
     ////////                                                 Table Creation                                                  //////////
     //////////////////////////////////////////////////////////////////////
@@ -54,6 +61,8 @@ public class V3__initial_schema extends TableCreationMigration {
     @Override
     protected void insertData() {
         systemBeanDefinitionTypeRepository.save(getSystemBeanDefinitionTypeList());
+        systemBeanModifierTypeRepository.save(getSystemBeanModifierTypeList());
+        systemBeanDefinitionRepository.save(getSystemBeanDefinitionList());
     }
 
     public List<SystemBeanDefinitionType> getSystemBeanDefinitionTypeList() {
@@ -67,6 +76,35 @@ public class V3__initial_schema extends TableCreationMigration {
 
     private SystemBeanDefinitionType createSystemBeanDefinitionType(String name, String description) {
         SystemBeanDefinitionType entity = new SystemBeanDefinitionType();
+        entity.setName(name);
+        entity.setDescription(description);
+        return entity;
+    }
+
+    private List<SystemBeanModifierType> getSystemBeanModifierTypeList() {
+        return asList(
+                getSystemBeanModifierType("public", "Java keyword used in a method or variable declaration. It signifies that the method or variable can be accessed by elements residing in other classes."),
+                getSystemBeanModifierType("private", "A Java keyword used in a method or variable declaration. It signifies that the method or variable can only be accessed by other elements of its class."),
+                getSystemBeanModifierType("final", "You define an entity once and cannot change it or derive from it later. More specifically: a final class cannot be subclassed, a final method cannot be overridden and a final variable cannot change from its initialized value."),
+                getSystemBeanModifierType("static", "A Java keyword used to define a variable as a class variable. Classes maintain one copy of class variables regardless of how many instances exist of that class. static can also be used to define a method as a class method.")
+        );
+    }
+
+    private SystemBeanModifierType getSystemBeanModifierType(String name, String description) {
+        SystemBeanModifierType entity = new SystemBeanModifierType();
+        entity.setName(name);
+        entity.setDescription(description);
+        return entity;
+    }
+
+    private List<SystemBeanDefinition> getSystemBeanDefinitionList() {
+        return asList(
+                getSystemBeanDefinition("Inversion Bean", "An inversion bean is a broad definition for various types of beans that are dynamically injected into the inversion context for use by the application.")
+        );
+    }
+
+    private SystemBeanDefinition getSystemBeanDefinition(String name, String description) {
+        SystemBeanDefinition entity = new SystemBeanDefinition();
         entity.setName(name);
         entity.setDescription(description);
         return entity;
